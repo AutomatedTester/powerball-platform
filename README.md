bc85831539e5d00ba5e5ef139243a213c4989f28
AutomatedTester <dburns@mozilla.com>
2011-10-20 10:27:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+=Powerball=
+An experiment in how to get people to help doing QA work with a scoring mechanism so that people can compete against
+friends and other contributors
---
+++ app.js
@@ -0,0 +1,97 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , Oauth = OAuth = require('oauth').OAuth
+  , sys = require('sys')
+  , fs = require('fs')
+  , RedisStore = require('connect-redis');
+
+
+var app = module.exports = express.createServer();
+var RedisStore = require('connect-redis')(express);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'showMeTheMoney', store: new RedisStore }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+var oa= new OAuth("https://twitter.com/oauth/request_token",
+                 "https://twitter.com/oauth/access_token", 
+                 'conkey', 'consecret', 
+                 "1.0A", 'http://localhost:3000/sessions/callback', "HMAC-SHA1");
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  if(!req.session.oauthAccessToken) {
+    res.redirect("/twitter");
+  } else {
+    res.render('index', {
+      title: 'Powerball'
+    });
+  }
+});
+
+
+app.get('/twitter', function(req, res){
+  oa.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      // TODO(David) error gracefully
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
+    }
+  });
+})
+  
+
+app.get('/sessions/callback', function(req, res){
+  oa.getOAuthAccessToken(req.session.oauthRequestToken, 
+                        req.session.oauthRequestTokenSecret, 
+                        req.query.oauth_verifier, 
+  function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    if (error) {
+      // TODO(David) error gracefully
+    } else {
+      // TODO(David) Push oauthAccessToken and oauthAccessTokenSecret to datastore for offline use
+      req.session.oauthAccessToken = oauthAccessToken;
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+
+      // Right here is where we would write out some nice user stuff
+      oa.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
+        if (error) {
+          // TODO(David) error gracefully
+        } else {
+          data1 = JSON.parse(data);
+          // TODO(David) store user details
+          req.session.twitterScreenName = data1["screen_name"];
+          res.send('You are signed in: ' + req.session.twitterScreenName + ' or ' + data1["screen_name"]);
+        }
+      });
+    }
+  });
+});
+
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
