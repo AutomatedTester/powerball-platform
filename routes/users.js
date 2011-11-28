38336b2dfa131368d3819dde8ec347921ca5ffee
AutomatedTester <dburns@mozilla.com>
2011-11-28 22:31:43 +0000

    removing unneeded params from object

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var DataProvider = require('../dataprovider').DataProvider;
 
 module.exports = function(app){
-  var dataProvider = new DataProvider('localhost', 27017);
+  var dataProvider = new DataProvider();
 
   app.get('/user/:name', function(req, res, next){
     dataProvider.findUser(req.params.name, function(error, user) {
