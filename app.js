edbd6da96ba9edf93db492df144f6cc18d779447
AutomatedTester <dburns@mozilla.com>
2011-12-04 15:17:27 +0000

    lets set the logging up properly this time!

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
-  app.use(express.logger({format: ':method :url'}));
+  app.use(express.logger());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'showMeTheMoney', store: new RedisStore }));
